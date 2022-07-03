446bf7c8af5ab4c449016fd5676a47ad38366093
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2022-07-03 23:07:29 +0200

    Add example of http handler

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { Receiver } from "@slack/bolt";
+
 require("dotenv").config();
 
 import { ChatBot } from "./types";
@@ -5,8 +7,10 @@ import { ChatBot } from "./types";
 export const createApp = (config: {
   slackBotToken: string;
   slackSigningSecret: string;
+  receiver: Receiver | undefined;
 }): ChatBot =>
   new ChatBot({
     token: config.slackBotToken,
     signingSecret: config.slackSigningSecret,
+    receiver: config.receiver,
   });
