fc5072de5032d34ef8e0cb26b45700f05c93d0fd
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2023-09-30 23:00:28 +0200

    remove unused redis

--- src/bot/constants.ts
+++ src/bot/constants.ts
@@ -3,8 +3,6 @@ import { Redis } from '@upstash/redis';
 export const token = process.env.SLACK_BOT_TOKEN as string;
 export const signingSecret: string = process.env.SLACK_SIGNING_SECRET as string;
 
-const redisURL = process.env.UPSTASH_REDIS_REST_URL as string;
-const redisToken = process.env.UPSTASH_REDIS_REST_TOKEN as string;
 const allowedToken = process.env.WEBHOOK_TOKEN as string;
 export const defaultChannel = process.env.SLACK_WEBHOOK_CHANNEL || '#random';
 export const allowedTokens = [allowedToken];
@@ -18,14 +16,3 @@ if (!token) {
 if (!signingSecret) {
   throw new Error('Missing SLACK_SIGNING_SECRET');
 }
-if (!redisURL) {
-  throw new Error('Missing redis URL');
-}
-if (!redisToken) {
-  throw new Error('Missing redis Token');
-}
-
-export const redis = new Redis({
-  url: redisURL,
-  token: redisToken,
-});
